f8ee89de8278459cb505977392151ac94b7f4731
aukey2 <aukey2@illinois.edu>
2019-12-03 15:02:11 -0600

    Gravity issue fixed

--- Assets/Scripts/ControllerScript.cs
+++ Assets/Scripts/ControllerScript.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class ControllerScript : MonoBehaviour
 {
+    public GameObject go;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +14,20 @@ public class ControllerScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        RaycastHit hit;
         transform.rotation = OVRInput.GetLocalControllerRotation(OVRInput.Controller.RTrackedRemote);
+
+        if (Physics.Raycast(transform.position, transform.forward, out hit))
+        {
+            if (hit.collider != null)
+            {
+                if (go != hit.collider.gameObject)
+                {
+                    go = hit.transform.gameObject;
+                    go.SendMessage("OnVREnter");
+                }
+            }
+        }
     }
+
 }
